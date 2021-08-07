9960031b414f1ba8e188adf1314804edd27573ad
ngochoanit <phamngochoan.it@gmail.com>
2021-08-07 21:43:07 +0700

    createboard API

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { connectDB } from '*/config/mongodb'
 import { env } from '*/config/environtment'
+import { apiV1 } from '*/routes/v1'
 
 connectDB()
     .then(() => console.log('Connected successfully to database server'))
@@ -12,15 +13,11 @@ connectDB()
 
 const bootServer = () => {
     const app = express()
-
-    app.get('/broad', async (req, res) => {
-        res.send('Hello World')
-    })
-    app.get('/column', async (req, res) => {
-        res.send('Hello World')
-    })
-
+    // Enable req.body data
+    app.use(express.json())
+    // Use Apis V1
+    app.use('/v1', apiV1)
     app.listen(env.APP_PORT, env.APP_HOST, () => {
-        console.log(`Example app listening at ${env.APP_HOST}:${env.APP_PORT}`)
+        console.log(`Hello Trello app,I'm running : ${env.APP_HOST}:${env.APP_PORT}`)
     })
 }
